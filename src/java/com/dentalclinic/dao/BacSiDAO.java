cd0df3a55b587fe48416accb833a1207777ce400
Hungp <hungphamduy456654@gmail.com>
2026-04-23 21:19:43 +0700

    update bacSiDAO

--- src/java/com/dentalclinic/dao/BacSiDAO.java
+++ src/java/com/dentalclinic/dao/BacSiDAO.java
@@ -1,54 +1,49 @@
-
 package com.dentalclinic.dao;
 
 import com.dentalclinic.model.BacSi;
 import com.dentalclinic.model.TaiKhoan;
 import com.dentalclinic.utils.DBConnection;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BacSiDAO {
+
+    private static final String GET_ALL_BAC_SI = 
+        "SELECT bs.BacSi_ID, bs.TaiKhoan_ID, bs.AnhDaiDien, bs.TrinhDo, tk.HoTen, " +
+        "(SELECT TOP 1 ck.TenChuyenKhoa FROM BacSi_ChuyenKhoa bsck " +
+        " JOIN ChuyenKhoa ck ON bsck.ChuyenKhoa_ID = ck.ChuyenKhoa_ID " +
+        " WHERE bsck.BacSi_ID = bs.BacSi_ID) AS TenChuyenKhoa " +
+        "FROM BacSi bs " +
+        "JOIN TaiKhoan tk ON bs.TaiKhoan_ID = tk.TaiKhoan_ID " +
+        "ORDER BY bs.BacSi_ID";
+
     public List<BacSi> getAllBacSi() {
+        List<BacSi> list = new ArrayList<>();
 
-        List<BacSi> list =new ArrayList<>();
-        String sql ="SELECT"
-                        +"bs.BacSi_ID, "
-                        +"bs.TaiKhoan_ID, "
-                        +"bs.AnhDaiDien, "
-                        +"bs.TrinhDo, "
-                        +"tk.HoTen, "
-                        +"ck.TenChuyenKhoa "
-                    +"FROM BacSi bs "
-                    +"JOIN TaiKhoan tk ON bs.TaiKhoan_ID = tk.TaiKhoan_ID "
-                    +"JOIN BacSi_ChuyenKhoa bsck ON bs.BacSi_ID = bsck.BacSi_ID "
-                    +"JOIN ChuyenKhoa ck ON bsck.ChuyenKhoa_ID = ck.ChuyenKhoa_ID "
-                    +"ORDER BY bs.BacSi_ID";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(GET_ALL_BAC_SI);
+             ResultSet rs = ps.executeQuery()) {
 
-            try (Connection conn = DBConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
+                TaiKhoan tk = new TaiKhoan();
+                tk.setTaiKhoanID(rs.getInt("TaiKhoan_ID"));
+                tk.setHoTen(rs.getString("HoTen"));
+
                 BacSi bs = new BacSi();
                 bs.setBacSiID(rs.getInt("BacSi_ID"));
                 bs.setTaiKhoanID(rs.getInt("TaiKhoan_ID"));
                 bs.setAnhDaiDien(rs.getString("AnhDaiDien"));
                 bs.setTrinhDo(rs.getString("TrinhDo"));
                 bs.setTenChuyenKhoa(rs.getString("TenChuyenKhoa"));
-                TaiKhoan tk = new TaiKhoan();
-                tk.setTaiKhoanID(rs.getInt("TaiKhoan_ID"));
-                tk.setHoTen(rs.getString("HoTen"));
+
                 bs.setTaiKhoan(tk);
+
                 list.add(bs);
-                }
-            } catch (SQLException e) {
-            System.err.println("Lỗi khi lấy danh sách bác sĩ: " + e.getMessage());
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return list;
     }
 }
-        
--- src/java/com/dentalclinic/dao/LichLamViecDAO.java
+++ src/java/com/dentalclinic/dao/LichLamViecDAO.java
@@ -1,5 +1,9 @@
 package com.dentalclinic.dao;
 
+import com.dentalclinic.model.BacSi;
+import com.dentalclinic.model.BenhNhan;
+
+
 public class LichLamViecDAO {
     
 }
